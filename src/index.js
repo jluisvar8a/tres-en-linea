6af5385fd46b9a58c939b5ee05f0e887ebad9e30
Jorge Luis Vargas <jorgeluvar96@gmail.com>
2019-02-24 18:30:32 -0500

    model database and views

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ const app = express()
 
 // Setting
 app.set('port', process.env.PORT || 3000)
-app.set('views', path.join(__dirname, 'views'))
+//app.use(express.static('views'))
+//app.engine('html');
+
+app.use(express.static(__dirname + '/views'))
+app.use(express.static(__dirname + '/public'))
+//app.set('views', path.join(__dirname, 'views'))
+//app.use('views', express.static(__dirname, 'views'))
+//app.set('view engine', 'html')
 
 // Routes
 app.use(require('./routes/index'))
-
-
 
 // Server is listening
 app.listen(app.get('port'), () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req, res) => {
-    res.render('index')
+    //res.send('hola')
+   //res.sendFile('/apps/tres-en-linea/src/views/index.html')
+   res.render('index.html')
+   res.render('index.css')
 })
 
 module.exports = router
